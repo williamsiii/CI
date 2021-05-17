4d60d618c152f29c53d63576c81e3195f371cae9
ArtemN <a.nosov@hh.ru>
2021-05-17 20:33:57 +0300

    next best

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+// BEGIN
+function upVersion(path, type = 'patch') {
+    const data = JSON.parse(fs.readFileSync(path, 'utf8'));
+    const [major, minor, patch] = data.version.split('.').map(Number);
+    const doUpVersion = (version) => fs.writeFileSync(
+        path,
+        JSON.stringify({ ...data, version }),
+    );
+
+    switch (type) {
+        case 'major': {
+            doUpVersion(`${major + 1}.0.0`);
+            break;
+        }
+        case 'minor': {
+            doUpVersion(`${major}.${minor + 1}.0`);
+            break;
+        }
+        case 'patch': {
+            doUpVersion(`${major}.${minor}.${patch + 1}`);
+            break;
+        }
+        default: {
+            throw new Error(`Unknown version type: ${type}`);
+        }
+    }
+}
+
+// END
+
+module.exports = { upVersion };
